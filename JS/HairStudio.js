af714518ac781d597fbec05fd4019a20a441e592
Robert Daniels Jr <broozerdaniels@yahoo.com>
2023-05-16 23:42:11 -0400

    made a JS page and added items to the booking page

---
+++ JS/HairStudio.js
@@ -0,0 +1,26 @@
+
+function geek() {
+           
+    var doc = prompt("Please enter some text",
+        "GeeksforGeeks");
+   
+    if (doc != null) {
+        document.getElementById("g").innerHTML =
+            "Welcome to " + doc;
+    }
+}
+
+function showDropInfo() {
+    var sT = dropForm.dropSelect;
+    var pF = document.getElementById('pF');
+    var pS = document.getElementById('pS');
+    var pT = document.getElementById('pT');
+    pF.innerHTML = ('Selected option index: ' + sT.selectedIndex);
+    pS.innerHTML = ('Selected value: ' + sT[sT.selectedIndex].value);
+    pT.innerHTML = ('Selected text: ' + sT[sT.selectedIndex].text);
+ }
+
+ function BPHairCut() {
+    let haircut = document.getElementById;
+
+ }
